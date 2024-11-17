87054e8433b9d1eb19cc4a4d627c969839567be3
kmjak <tymkoron1221@icloud.com>
2024-11-18 02:35:09 +0900

    実際にプロジェクトを作るserviceを作成した

---
+++ src/services/database/projects/createProject.ts
@@ -0,0 +1,20 @@
+"use server"
+
+import ProjectType from "@/types/interface/project/ProjectType";
+import project_url from "@/lib/url/projects/projects_url";
+
+export default async function createProject({ id, ownerId, projectName, canvasSize, canvasData }: ProjectType): Promise<boolean> {
+  const canvasDataString = JSON.stringify(canvasData)
+  const res = await fetch(project_url!, {
+    method: "POST",
+    body: JSON.stringify({ id, ownerId, projectName, canvasSize, canvasDataString }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (res.ok) {
+    return true;
+  } else {
+    return false;
+  }
+}
