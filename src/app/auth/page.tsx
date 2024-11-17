b57de5a2006e8a65af2ffafeb71610ce61a6bb11
kmjak <tymkoron1221@icloud.com>
2024-11-18 02:42:25 +0900

    cryptoConfigがいらないのにpropsとして渡していたので削除した

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,9 @@
-import cryptoConfig from "@/lib/config/crypto/cryptoConfig";
 import AuthForm from "./components/AuthForm";
 
 export default function Page() {
   return (
     <div>
-      <AuthForm
-        cryptoConfig={cryptoConfig}
-      />
+      <AuthForm />
     </div>
   );
 }
