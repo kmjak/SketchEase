ff10ee83932a45ec6b05e28abdb1399e5a7f2d07
kmjak <tymkoron1221@icloud.com>
2024-11-25 02:48:58 +0900

    setCookieを作った

--- src/hooks/cookie/useCookie.ts
+++ src/hooks/cookie/useCookie.ts
@@ -1,10 +1,15 @@
 import getCookieUseCase from "@/usecase/cookie/getCookieUseCase"
+import setCookieUseCase from "@/usecase/cookie/setCookieUseCase"
 
 export default function useCookie (){
   const getCookie = async (name:string) => {
     return await getCookieUseCase({name})
   }
+  const setCookie = async (name:string, value:string, maxAge:number) => {
+    return await setCookieUseCase({name, value, maxAge})
+  }
   return {
     getCookie,
+    setCookie,
   }
 }
