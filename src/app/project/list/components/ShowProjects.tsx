bea20014edc87fb38a174ffb185bc646b484d3da
kmjak <tymkoron1221@icloud.com>
2024-12-01 01:29:18 +0900

    過去の作品を開く・編集ができる様にした

--- src/app/project/list/components/ShowProjects.tsx
+++ src/app/project/list/components/ShowProjects.tsx
@@ -3,14 +3,15 @@
 import useCookie from "@/hooks/cookie/useCookie";
 import ProjectType from "@/types/interface/project/ProjectType";
 import decryptUseCase from "@/usecase/crypto/decryptUseCase";
+import encryptUseCase from "@/usecase/crypto/encryptUseCase";
 import getAllProjectsUseCase from "@/usecase/project/getAllProjectsUseCase";
+import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function ShowProjects() {
   const [projects, setProjects] = useState<ProjectType[] | []>([]);
   const [ownerId, setOwnerId] = useState<string>("");
-  const { getCookie } = useCookie();
-  
+  const { getCookie, setCookie } = useCookie()
   useEffect(() => {
     const fetchOwnerId = async () => {
       const cipher_userId = await getCookie("id");
@@ -28,13 +29,23 @@ export default function ShowProjects() {
     fetchProjects();
   }, [ownerId]);
 
+  const openProject = async (projectId:string) => {
+    const cipher_projectId = await encryptUseCase({plain_text: projectId, mode: "cookie"});
+    await setCookie("projectId", cipher_projectId, 60*60*24);
+    redirect("/project/edit");
+  }
+
   return (
-    <section>
-      <ul>
+    <section className="p-8">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {projects.map((project) => {
           return (
-            <li key={project.id}>
-              <h2>{project.projectName}</h2>
+            <li
+              key={project.id}
+              onClick={() => openProject(project.id)}
+              className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow cursor-pointer border border-gray-200 hover:border-gray-400"
+            >
+              <h2 className="text-xl font-semibold text-gray-700">{project.projectName}</h2>
             </li>
           );
         })}
--- src/app/project/list/page.tsx
+++ src/app/project/list/page.tsx
@@ -3,7 +3,7 @@ import ShowProjects from "./components/ShowProjects";
 export default function Page() {
   return (
     <main>
-      <h1>プロジェクト一覧</h1>
+      <h1 className="text-2xl font-bold text-center py-6">プロジェクト一覧</h1>
       <ShowProjects />
     </main>
   );
