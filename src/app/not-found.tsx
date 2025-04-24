7dba34fc0d006828e3b75130c12ff7e0886a59c2
kmjak <tymkoron1221@icloud.com>
2025-04-25 01:46:45 +0900

    fix: ルートページにアクセスした時に認証画面に行くように推奨した

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 - Not Found!</h1>
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <h1 className="text-3xl">404 - Not Found!</h1>
+      <Link href="auth" className="text-lg underline text-blue-600">
+        認証をしてください
+      </Link>
     </div>
   );
 }
